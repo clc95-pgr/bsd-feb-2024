2affdc9a19dd014d7d93b8157d1537ad88fee048
Chris Crowder <christopher_l_crowder@progressive.com>
2024-02-21 16:03:59 -0500

    started work on second api

---
+++ my-projects/IssueTrackerApi/IssueTrackerApi/Controllers/IssuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTrackerApi.Controllers;
+
+public class IssuesController : ControllerBase
+{
+    [HttpPost("/issues")]
+    public async Task<ActionResult> AddIssueAsync([FromBody] IssuesRequest request)
+    {
+        var response = new IssueResponse(
+            request.Software,
+            request.Description,
+            DateTimeOffset.Now,
+            IssueStatus.Pending
+            );
+        return Ok(response);
+    }
+}
+
+
+public record IssuesRequest(
+    string Software,
+    string Description
+    );
+
+public record IssueResponse(
+    string Software,
+    string Description,
+    DateTimeOffset Logged,
+    IssueStatus Status
+    );
+
+public enum IssueStatus { Pending };
