843fbb62a71a3f83f812e2c4d73fc83ad15ce827
Chris Crowder <christopher_l_crowder@progressive.com>
2024-02-21 16:42:35 -0500

    more work

--- my-projects/IssueTrackerApi/IssueTrackerApi/Controllers/IssuesController.cs
+++ my-projects/IssueTrackerApi/IssueTrackerApi/Controllers/IssuesController.cs
@@ -1,19 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTrackerApi.Controllers;
 
-public class IssuesController : ControllerBase
+public class IssuesController(IDocumentSession session) : ControllerBase
 {
     [HttpPost("/issues")]
     public async Task<ActionResult> AddIssueAsync([FromBody] IssuesRequest request)
     {
         var response = new IssueResponse(
+            Guid.NewGuid(),
             request.Software,
             request.Description,
             DateTimeOffset.Now,
             IssueStatus.Pending
             );
+
+        session.Insert(response);
+        await session.SaveChangesAsync();
+
         return Ok(response);
+    }
+
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetAllIssues()
+    {
+        var response = await session.Query<IssueResponse>().ToListAsync();
+        return Ok(new IssuesResponseCollection(response));
+    }
+
+    [HttpGet("/issues/{id:guid}")]
+    public async Task<ActionResult> GetIssuesById(Guid id)
+    {
+        var response = await session.Query<IssueResponse>()
+            .Where(issue => issue.Id == id)
+            .SingleOrDefaultAsync();
+
+        if (response is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(response);
+        }
     }
 }
 
@@ -24,10 +54,14 @@ public record IssuesRequest(
     );
 
 public record IssueResponse(
+    Guid Id,
     string Software,
     string Description,
     DateTimeOffset Logged,
     IssueStatus Status
     );
 
+
 public enum IssueStatus { Pending };
+
+public record IssuesResponseCollection(IReadOnlyList<IssueResponse> Data);
