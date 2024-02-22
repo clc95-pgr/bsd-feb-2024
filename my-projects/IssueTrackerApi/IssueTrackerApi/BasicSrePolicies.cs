523c6d62641641fd1371b03ed163268bf0e69db0
Chris Crowder <christopher_l_crowder@progressive.com>
2024-02-22 13:50:01 -0500

    add policy files

---
+++ my-projects/IssueTrackerApi/IssueTrackerApi/BasicSrePolicies.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+
+namespace IssueTrackerApi;
+
+public class BasicSrePolicies
+{
+
+    public static IAsyncPolicy<HttpResponseMessage> GetDefaultRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(2, retryAttemptNumber => TimeSpan.FromSeconds(Math.Pow(2, retryAttemptNumber)));
+    }
+
+    public static IAsyncPolicy<HttpResponseMessage> GetDefaultCircuitBreaker()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30));
+    }
+}
---
+++ my-projects/IssueTrackerApi/IssueTrackerApi/Services/BusinessClockHttpService.cs
@@ -0,0 +1,15 @@
+﻿namespace IssueTrackerApi.Services;
+
+public class BusinessClockHttpService(HttpClient client)
+{
+    public async Task<SupportResponse?> GetCurrentSupportInformationAsync()
+    {
+        var response = await client.GetAsync("/support-info");
+        response.EnsureSuccessStatusCode();
+
+        var body = await response.Content.ReadFromJsonAsync<SupportResponse>();
+        return body;
+    }
+}
+
+public record SupportResponse(string name, string Phone);
